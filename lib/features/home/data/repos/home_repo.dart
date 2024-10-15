196b2263509da33406b459fe2f18bf6e50f161b6
DESKTOP-S7FFHIC\h <m.alamrousi.work2@gmail.com>
2024-10-15 14:04:37 +0300

    Implement fetch newest books method

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -3,6 +3,6 @@ import 'package:bookly_app/features/home/data/models/book_model.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<Either<Failures, List<BookModel>>> fetchBestSellerBox();
+  Future<Either<Failures, List<BookModel>>> fetchNewestBox();
   Future<Either<Failures, List<BookModel>>> fetchFeatureBox();
 }
--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,12 +1,33 @@
-import 'package:bookly_app/core/errors/failures.dart';
-import 'package:bookly_app/features/home/data/models/book_model.dart';
-import 'package:bookly_app/features/home/data/repos/home_repo.dart';
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:dartz/dartz.dart';
 
-class HomeRepoImpl implements   HomeRepo{
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/core/utils/api_services.dart';
+import 'package:bookly_app/features/home/data/models/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
+
+class HomeRepoImpl implements HomeRepo {
+  final ApiServices apiServices;
+  HomeRepoImpl({
+    required this.apiServices,
+  });
   @override
-  Future<Either<Failures, List<BookModel>>> fetchBestSellerBox() {
-    throw UnimplementedError();
+  Future<Either<Failures, List<BookModel>>> fetchNewestBox() async {
+    try {
+      var data = await apiServices.get(
+          endPoint:
+              'volumes?q=subject:programming&filterting=free-ebooks&orderBy=newest');
+
+      List<BookModel> books = [];
+
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+
+      return right(books);
+    } on Exception catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
